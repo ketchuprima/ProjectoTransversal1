82721adf6376ec537aae1be0350d241c3649f11a
ketchuprima <73467976+ketchuprima@users.noreply.github.com>
2020-11-03 12:56:14 +0100

    Intento de enviar correo

--- Pantallas/FinalizarCompra.php
+++ Pantallas/FinalizarCompra.php
@@ -10,6 +10,7 @@
     <div id="pedidoFinal">
         <ul id="listaFinal"></ul>
     </div>
+<<<<<<< Updated upstream
     <div id="formulario">
         <form action="confirmar.php" method="post" name="factura" id="factura">
             <div>
@@ -35,5 +36,30 @@
         </form>
     </div>
     <script src="../Funciones/FinalizarCompra.js"></script>
+=======
+    <form action="confirmar.php" method="post" name="factura" id="factura">
+        <div>
+            <label for="nombre" class="label">Nombre</label>
+            <input name="nombre" type="text" id="nombre">
+            <span id="errornombre"></span>
+        </div>
+        <div>
+            <label for="telefono" class="label">Teléfono</label>
+            <input name="telefono" type="text" id="telefono" onkeyup="numeroCorrecto()">
+            <span id="errortelefono"></span>
+        </div>
+        <div>
+            <label for="correo" class="label">Correo electrónico</label>
+            <input name="correo" type="text" id="correo" onkeyup="validarEmail()">
+            <span id="errorcorreo"></span>
+        </div>
+        <input name="pedido" id="pedido" type="text" style="visibility: hidden">
+        <div>
+            <input type="button" onclick="atras()" name="retroceder" id="retroceder" value="Retroceder">
+            <input type="submit" name="enviar" id="enviar" value="Enviar">
+        </div>
+    </form>
+    <script src="../Funciones/CompFormularioCompra.js"></script>
+>>>>>>> Stashed changes
 </body>
 </html>
--- Pantallas/confirmar.php
+++ Pantallas/confirmar.php
@@ -7,6 +7,41 @@
 <body>
     <?php
 
+    date_default_timezone_set('Etc/UTC');
+
+    require "../libs/PHPMailer-5.2-stable/PHPMailerAutoload.php";
+    $mail = new PHPMailer;
+    
+    $mail->IsSMTP();
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = 'html';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth = true;
+    $mail->Username = "miqserjoe@inspedralbes.cat";
+    $mail->Password = "ausias12";
+    
+
+
+    $mail->setFrom("miqserjoe@gmail.com", "MiqSerJoe"); //remitente
+    $mail->addAddress($_POST["correo"]);
+    $mail->Subject = $_POST["nombre"];
+    $mail->Body = "Su pedido ha sido recibido";
+
+    $mail -> isHTML(true);
+
+    if ($mail->Send()) {
+        echo'<script type="text/javascript">
+            alert("Enviado Correctamente");
+            </script>';
+        
+    } else {
+        echo'<script type="text/javascript">
+            alert("NO ENVIADO, intentar de nuevo");
+            </script>';
+    }
+
     $fecha = date("d-m-Y");
 
 
