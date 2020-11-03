3da55a6a68d2e4f328b83056039e92f17070221b
elmaykhel <73468071+elmaykhel@users.noreply.github.com>
2020-11-03 12:54:52 +0100

    diseño inacabado y feo

---
+++ Pantallas/header.php
@@ -0,0 +1,1 @@
+<img src="../Sources/LogoInstitut.png" id="imagen"> 
--- Pantallas/principal.php
+++ Pantallas/principal.php
@@ -4,17 +4,22 @@
     <meta charset="UTF-8">
     <title>Landing Page</title>
     <link rel="stylesheet" href="../Estilos/Principal.css">
+    <link rel="stylesheet" href="../Estilos/pagina.css">
 </head>
 <body>
-<div id="layer">
-    <img id="fotoInsti" src="../Sources/LogoInstitut.png">
-    <div id="lineaCentral">
-        <input id="comanda" type="image" onclick="location.href='comprobacio.php'" src="../Sources/LogoPedido.png">
-        <input id="lista" type="image" onclick="location.href='../Administracion/Administracion.php'" src="../Sources/LogoLista.png">
+    <div class="header">
+        <?php include("./header.php") ?>
     </div>
-    <br/>
-</div>
-   
-
+    <div id="layer">
+        <img id="fotoInsti" src="../Sources/LogoInstitut.png">
+        <div id="lineaCentral">
+            <input id="comanda" type="image" onclick="location.href='comprobacio.php'" src="../Sources/LogoPedido.png">
+            <input id="lista" type="image" onclick="location.href='../Administracion/Administracion.php'" src="../Sources/LogoLista.png">
+        </div>
+        <br/>
+    </div>
+    <div class="footer">
+        <?php include("./footer.php") ?>
+    </div>
 </body>
 </html>
