59c337c0ea7ed5eba6e6ee65f2126d8c2a2a0932
Sergio <a18sergribra@inspedralbes.cat>
2020-11-03 17:29:10 +0100

    Error mas bonito cuando hay cookie

--- Pantallas/error.php
+++ Pantallas/error.php
@@ -2,20 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
 </head>
 <body>
 
-    <h1>ERROR: Ja has fet una comanda avui</h1>
+    <!--<h1>ERROR: Ja has fet una comanda avui</h1>
     <form method="post" action="error.php">
         <input type="submit" name="inici" value="inici"/>
-    </form>
+    </form>-->
 
     <?php
-        if(isset($_POST['inici'])){
+        /*if(isset($_POST['inici'])){
             header("Location: principal.php");
-        }
+        }*/
     ?>
+    
+    <link rel="stylesheet" type="text/css" href="../libs/sweetalert/dist/sweetalert2.min.css">
+    <script type="text/javascript" src="../libs/sweetalert/dist/sweetalert2.min.js" ></script>
+    <script type="text/javascript">
+        Swal.fire({
+            title: 'Error',
+            text: 'Ya has realizado un pedido hoy',
+            icon: 'error',
+            confirmButtonText: "volver a inicio",
+        }).then((result)=>{
+            if(result.isConfirmed){
+                window.location.href="principal.php";
+            }
+        })
+    </script>
+
+    
 
 </body>
 </html>
