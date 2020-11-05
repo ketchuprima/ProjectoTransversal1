f15c393fdfd9c59e9ec8b41ae6764c9bf80dffc6
elmaykhel <73468071+elmaykhel@users.noreply.github.com>
2020-11-05 10:43:53 +0100

    include admin

--- Administracion/Administracion.php
+++ Administracion/Administracion.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "../Funciones/bootstrap.php"?>
     <link rel="stylesheet" type="text/css" href="../Estilos/Administracion.css">
 </head>
 <body>
