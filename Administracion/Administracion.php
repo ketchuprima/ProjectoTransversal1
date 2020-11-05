7023d8aded442850ffba8f5126a7ad7ff453f6cd
Sergio <a18sergribra@inspedralbes.cat>
2020-11-05 10:34:45 +0100

    xd otra vez

--- Administracion/Administracion.php
+++ Administracion/Administracion.php
@@ -15,7 +15,7 @@
                 while (($archivo = readdir($gestor)) !== false)  {
                     $ruta_completa = $ruta . "/" . $archivo;
                     if ($archivo != "." && $archivo != ".." && !strpos($archivo, "php")){
-                        echo "<li><input class=btn btn-outline-info type=button value=$archivo onclick=location.href='../Pedidos/$archivo'></li>";
+                        echo "<li><input class=btn-outline-info type=button value=$archivo onclick=location.href='../Pedidos/$archivo'></li>";
                     }
                 }
                 closedir($gestor);
